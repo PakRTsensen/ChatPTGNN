b8b602bc20536f3d3b02e5e52400e5399292987e
PakRTsensen <96939888+PakRTsensen@users.noreply.github.com>
2023-12-26 02:29:09 +0700

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,65 +1,47 @@
-import { kv } from '@vercel/kv'
+// app/api/chat/route.ts
+
+import { OpenAI } from 'openai'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
-import OpenAI from 'openai'
 
-import { auth } from '@/auth'
-import { nanoid } from '@/lib/utils'
-
-export const runtime = 'edge'
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+const fireworks = new OpenAI({
+  baseURL: 'https://api.fireworks.ai/inference/v1',
+  apiKey: process.env.FIREWORKS_API_KEY!
 })
 
-export async function POST(req: Request) {
-  const json = await req.json()
-  const { messages, previewToken } = json
-  const userId = (await auth())?.user.id
-
-  if (!userId) {
-    return new Response('Unauthorized', {
-      status: 401
-    })
-  }
-
-  if (previewToken) {
-    openai.apiKey = previewToken
-  }
-
-  const res = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages,
-    temperature: 0.7,
-    stream: true
-  })
-
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
+// Build a prompt from the messages
+function buildPrompt(messages: { content: string; role: 'system' | 'user' | 'assistant' }[]) {
+  return (
+    messages
+      .map(({ content, role }) => {
+        if (role === 'user') {
+          return `Human: ${content}`;
+        } else {
+          return `Assistant: ${content}`;
+        }
       })
-    }
+      .join('\n\n') + 'Assistant:'
+  );
+}
+
+export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
+  const { messages } = await req.json();
+
+  // Request the Fireworks API for the response based on the prompt
+  const response = await fireworks.completions.create({
+    model: 'accounts/fireworks/models/mixtral-8x7b',
+    stream: true,
+    prompt: buildPrompt(messages),
+    max_tokens: 4000,
+    temperature: 0.5,
+    top_p: 1,
+    frequency_penalty: 1,
   })
 
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response)
+
+  // Respond with the stream
   return new StreamingTextResponse(stream)
 }
+
