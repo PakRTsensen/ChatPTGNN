d16f65d635c872c72982863ccb4e011a37bd21b3
PakRTsensen <96939888+PakRTsensen@users.noreply.github.com>
2023-12-26 02:01:37 +0700

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,2 @@
-import { Auth } from "@auth/core"
-import GitHub from "@auth/core/providers/github"
-
-const request = new Request(origin)
-const response = await Auth(request, {
-  providers: [GitHub({ clientId: 936c43ba2cb38edae571, clientSecret: 82a6ce4085a29fa30374bee0c30975515b03e46a })],
-})
+export { GET, POST } from '@/auth'
+export const runtime = 'edge'
